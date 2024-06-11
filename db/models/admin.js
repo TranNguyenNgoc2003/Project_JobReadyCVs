87becb4e6e3eea7cd08c90efe513e3f348051dcc
Davylow <116535558+Davylow@users.noreply.github.com>
2024-06-11 07:18:38 +0700

    Add files via upload

---
+++ db/models/admin.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const AdminSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: false, 
+  },
+  address: {
+    type: String,
+    required: false,
+  },
+  phoneNumber: {
+    type: String,
+    required: false,
+  },
+  dateOfBirth: {
+    type: Date,
+    required: false,
+  },
+});
+
+module.exports = mongoose.model('Admin', AdminSchema);
